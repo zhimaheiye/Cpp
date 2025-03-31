ce32be40a69a10d93b8ece3f3bf38f4cefe7a8bc
zhimaheiye <sxy1070518694@qq.com>
2025-03-31 14:36:30 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,24 +2,31 @@
 #include <stdio.h>
 #include<string.h>
 
-struct Stu
-{
-    char name[20];
-    int age;
-    char sex[10];
-    char tele[12];
-};//模板
-void print(struct Stu* ps)
-{
-    //printf("%s %d %s %s", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);方法一
 
-    printf("%s %d %s %s", ps->name, ps->age, ps->sex, ps->tele);
-}
 int main()
 {
-    struct Stu s = { "zhangsan",20,"nan","114514" };//创建一个具体的对象
-    //printf("%s %d %s %s", s.name, s.age, s.sex, s.tele);
-    print(&s);
+   /* char arr1[] = "abc";
+    char arr2[] = { 'a','b','c'};
+    char arr3[] = { 'a','b','c','\0','d'};
+
+    printf("%s\n", arr1);
+    printf("%s\n", arr2);
+    printf("%s\n", arr3);
+    printf("%zd\t", strlen(arr1));
+    printf("%zd\t", strlen(arr2));
+    printf("%zd\t", strlen(arr3));*/
+
+    printf("1\t");
+    printf("12\t");
+    printf("123\t");
+    printf("1234\t");
+    printf("12345\t");
+    
+
+   
+  
+
+
     return 0;
 }
 
