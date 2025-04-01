a8b595fc1c94404bcf6b4d0eb3281e311633e006
zhimaheiye <sxy1070518694@qq.com>
2025-04-01 12:11:23 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdbool.h>
 #include<string.h>
 
 
@@ -16,12 +17,66 @@ int main()
     printf("%zd\t", strlen(arr2));
     printf("%zd\t", strlen(arr3));*/
 
-    printf("1\t");
-    printf("12\t");
-    printf("123\t");
-    printf("1234\t");
-    printf("12345\t");
+    //printf("1\t");
+    //printf("12\t");
+    //printf("123\t");
+    //printf("1234\t");
+    //printf("12345\t");
+    //
+
+    //printf("\a");//œÏ¡À
+    //printf("%zd",strlen("c:\test\198\abc\test.c"));
+    //getchar();
+
+    //_Bool islike = true;
+    /*bool islike = true;
+    if (islike) 
+        printf("yes");
+    else 
+        printf("No");*/
+  /*  short s = 2;
+    int b = 10;*/
+    //printf("%zd\n", sizeof(s = b + 1));
+    ////                       int   int
+    ////                           int
+    ////                   short
+    //printf("s=%d\n", s);
+
     
+    //printf("%d", a);
+    /*int a = (int)3.14;
+    printf("%d", a);
+    */
+    //printf("hello\n");
+    
+    /*int ch = getchar();
+    printf("%c\n",ch);
+    putchar(ch);*/
+
+    //printf("%+d\n", 123);
+    //printf("%5d\n", 123);
+
+    //printf("%*.*f\n", 10, 2, 123.45);
+    /*printf("%s\n", "abcdef");
+    printf("%.5s\n", "abcdef");*/
+
+    /*int a = 0;
+    
+    printf("%d", a);*/
+    
+    EOF;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int r = scanf_s("%d%d%d", &a,&b,&c);
+    printf("%d%d%d\n", a, b, c);
+    printf("%d", r);
+
+
+
+
+
+
 
    
   
