8a924da32ccfd4a53b4a8e3c785cdf312b3074d5
zhimaheiye <sxy1070518694@qq.com>
2025-04-08 18:21:01 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdbool.h>
 #include<string.h>
@@ -64,19 +65,93 @@ int main()
     
     printf("%d", a);*/
     
-    EOF;
+  /*  EOF;
     int a = 0;
     int b = 0;
     int c = 0;
     int r = scanf_s("%d%d%d", &a,&b,&c);
     printf("%d%d%d\n", a, b, c);
-    printf("%d", r);
+    printf("%d", r);*/
 
 
+  /*  int num = 0;
+    scanf_s("%d", &num);
+    int i = 0;
+    while (i < num) {
+        printf("%c", '*');
+        i++;
+    }*/
+    /*char arr[20];
+    scanf_s("%s", arr);
+    printf("%s\n", arr);
+*/
 
 
+    /*int a = 1;
+    scanf_s("%d", &a);
+    printf("%d", a);*/
+
+   /* int a = 0;
+    scanf_s("%d", &a);
+    if (a % 2 == 1) {
+        printf("%d", a);
+    }*/
+    /*int a = 0;
+    int b = 2;
+    if (a == 1)
+        if (b == 2)
+            printf("hehe\n");
+        else
+            printf("haha\n");
+    return 0;*/
+    //int a = 0;
+    
+    //if (a = 1)
+    //    printf("哦不对不对");
+    //else
+    //    printf("哦对的对的");
+    /*int a = 10;
+    int b = 2;
+    int max = (a > b) ? a : b;
+    printf("%d", max);*/
+    /*int year = 0;
+    scanf_s("%d", &year);
+    if (year % 4 == 0 && year % 100 != 0) {
+    printf("闰年");
+    }
+    else if (year % 400 == 0) {
+    printf("闰年");
+}
+    else {
+    printf("平年");
+    }*/
+//int i = 0, a = 0, b = 2, c = 3, d = 4;
+//i = a++ || ++b || d++;
+//printf("%d%d%d%d", a, b, c, d);
+     /*char ch;
+     scanf_s(" %c\n", &ch);
+     printf("%c", ch);
+     printf("前面没有空格");*/
 
 
+     //char arr[2];
+     //scanf("%s", arr);//往数组里写入不用取地址符号&，因为数组本身就是地址
+     //printf("%s", arr)
+     /*int a = 0;
+     int b = 0;
+     scanf("%d-%d", &a, &b);
+     printf("%d %d", a, b);*/
+
+int a = 3;
+if (a > 1) {
+    printf("a > 1");
+}
+else if (a > 2) {
+    printf("a>2");
+}
+else {
+    printf("啥也不是");
+}
 
    
   
